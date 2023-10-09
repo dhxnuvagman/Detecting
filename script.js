2cbb6052e7b3ab9f400a618d462df09daf99f8c4
dhxnuvagman <130652450+dhxnuvagman@users.noreply.github.com>
2023-10-09 05:56:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-function checkinternet(){
-const img = new Image();
-var timestamp=Date.now();
-  img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6COcVph5YeFgDsIBDdZk0gQ1xIDybsBzZ8A&usqp=CAU?timestamp=${timestamp}";
-
-  img.onload = function () {
-document.getElementById("content").style.display="block";
-document.getElementById("internet").style.display="none";
-  };
-
-  img.onerror = function () {
-    document.getElementById("content").style.display="none";
-    document.getElementById("internet").style.display="block";
-  };}
-checkinternet();
-
 var lin=document.getElementById("link")
 const inst=document.getElementById("insta")
 const fac=document.getElementById("face")
@@ -22,40 +6,34 @@ const wha=document.getElementById("whats")
 var linktext=document.getElementById("linktext")
 
 function linkrun(){
-  checkinternet();
-  console.log("You clicked link");
+  
   inst.style.backgroundColor="white";
   fac.style.backgroundColor="white";
   wha.style.backgroundColor="white";
   lin.style.backgroundColor="#9933ff";
-  document.getElementById("forlink").style.display="block";
+  
 }
 function instrun(){
-  checkinternet();
-  console.log("You clicked Instagram")
+  
   lin.style.backgroundColor="white";
   fac.style.backgroundColor="white";
   wha.style.backgroundColor="white";
   inst.style.backgroundColor="#9933ff";
-  document.getElementById("forlink").style.display="none";
+
 }
 function facrun(){
-checkinternet();
-  console.log("You clicked Facebook")
+  
   inst.style.backgroundColor="white";
   lin.style.backgroundColor="white";
   wha.style.backgroundColor="white";
   fac.style.backgroundColor="#9933ff";
-  document.getElementById("forlink").style.display="none";
+  
 }
 function wharun(){
-  checkinternet();
-  console.log("You clicked WhatsApp")
   inst.style.backgroundColor="white";
   fac.style.backgroundColor="white";
   lin.style.backgroundColor="white";
   wha.style.backgroundColor="#9933ff"
-  document.getElementById("forlink").style.display="none";
 }
 function searchit(){
   var a=linktext.value;
